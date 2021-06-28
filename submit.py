d258260e6aeac5143178bb06bbe0091e847829d6
Alanasdw <alanasdw@gmail.com>
2021-06-28 15:31:01 +0800

    just adding the python testing things

--- submit.py
+++ submit.py
@@ -1,20 +1,18 @@
 from celery import Celery
 import subprocess
-import time
-from datetime import datetime
 
-app = Celery('tasks', broker='redis://localhost', result_backend='redis://localhost')
-app.config_from_object('celeryconfig')
+app = Celery('tasks',
+    broker='redis://localhost:6379',
+    backend='redis://localhost:6379'
+)
+
+app.conf.broker_transport_options = {
+    'queue_order_strategy': 'priority',
+}
 
 @app.task
 def add(x, y):
-    time.sleep(1)
-    return datetime.now()
-
-@app.task
-def important(x, y):
-    time.sleep(10)
-    return datetime.now()
+    return x + y
 
 @app.task
 def run( CONTAINER_PATH, LANG_ID, COMPILED, INPUT, OUTPUT, ERROR, TIME_LIMIT, MEMORY_LIMIT, FILE_LIMIT, SECCOMP_STRING):
@@ -31,4 +29,21 @@ def execute( source_name):
     #./run.sh CONTAINER_PATH LANG_ID COMPILED INPUT OUTPUT ERROR TIME_LIMIT MEMORY_LIMIT FILE_LIMIT SECCOMP_STRING
     # call something like gcc and the arguments inside
     return subprocess.call(source_name)
-    
+
+# highest priority 255
+# lowest/default priority 0
+
+if __name__ == '__main__':
+    #CONTAINER_PATH, LANG_ID, COMPILED, INPUT, OUTPUT, ERROR, TIME_LIMIT, MEMORY_LIMIT, FILE_LIMIT, SECCOMP_STRING
+    for i in range(10):
+        ar = add.apply_async((5456, 2878), priority = 90)
+    name = "test.c"
+    answer = compile.apply_async([name], priority = 10)
+
+    running = execute.apply_async(["./a.out"], priority = 5)
+
+
+    print( ar.get())
+    print( answer.get())
+    print( running.get())
+
