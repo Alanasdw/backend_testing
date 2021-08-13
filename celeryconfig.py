35b177b3a8425f43b514a4434fb1737257618250
Alanasdw <alanasdw@gmail.com>
2021-08-13 17:07:10 +0800

    Fix: tweeked the celeryconfig

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,13 +1,15 @@
 
-# go celery config
-task_serializer = 'json'
-accept_content = ['json']  # Ignore other content
-result_serializer = 'json'
-enable_utc = True
-task_protocol=1
-# go celery config end
+# # go celery config
+# task_serializer = 'json'
+# accept_content = ['json']  # Ignore other content
+# result_serializer = 'json'
+# enable_utc = True
+# task_protocol=1
+# # go celery config end
 
-broker_url = 'redis://'
-#result_backend = 'rpc://'
+broker_url = 'redis://localhost:6379'
+result_backend = 'redis://localhost:6379'
+
+broker_transport_options = {'queue_order_strategy': 'priority',}
 
 #timezone = 'Europe/Oslo'
